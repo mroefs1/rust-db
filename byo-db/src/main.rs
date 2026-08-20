2304bbe523e36b9757c9c7dd2ce025887ed58075
mroefs1 <michael_roefs@yahoo.com>
2026-08-20 11:39:33 -0400

    add basic file structure

--- byo-db/src/main.rs
+++ byo-db/src/main.rs
@@ -1,15 +1,1 @@
-use std::fs::File;
-use std::io::{self, Read};
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    let mut f = File::open("hello.txt")?;
-    let mut s = String::new();
-    f.read_to_string(&mut s)?;
-    Ok(s)
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let s: Result<String, io::Error> = read_username_from_file()?;
-    println!("{}", s);
-    Ok(())
-}
+fn main() {}
